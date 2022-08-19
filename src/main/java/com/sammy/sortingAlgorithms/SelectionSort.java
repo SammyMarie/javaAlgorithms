8d2feef3d076dc044d6ee022fd01276b2a3a0c45
SammyMarie <sml.ifere@gmail.com>
2022-08-19 12:59:57 +0100

    Adds code for Selection sort algorithm

--- src/main/java/com/sammy/sortingAlgorithms/SelectionSort.java
+++ src/main/java/com/sammy/sortingAlgorithms/SelectionSort.java
@@ -1,2 +1,26 @@
-package com.sammy.sortingAlgorithms;public class SelectionSort {
+package com.sammy.sortingAlgorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static int[] selectSort(int[] numberArray){
+        for(int index = 0; index < numberArray.length - 1; index++){
+            int minIndex = index;
+            for(int secondIndex = index + 1; secondIndex < numberArray.length; secondIndex++){
+                if(numberArray[secondIndex] < numberArray[minIndex]){
+                    minIndex = secondIndex;
+                }
+                int tempValue = numberArray[minIndex];
+                numberArray[minIndex] = numberArray[index];
+                numberArray[index] = tempValue;
+            }
+        }
+        return numberArray;
+        
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 2, 8, 6, 7, 3};
+        System.out.println(Arrays.toString(selectSort(numbers)));
+    }
 }
